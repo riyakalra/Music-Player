fd18980068b2f000b1f408c8699fd0fdc0a711fe
riyakalra <riyakalra59@gmail.com>
2025-05-27 14:33:19 +0530

    add search bar in navbar

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
-import "./index.css"
+import React from "react";
+import "./index.css";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 export default function Navbar() {
-    const userFirstName = "Riya";
-    const intial = userFirstName.charAt(0).toUpperCase();
-    return (
-        <nav className="navbar">
-            <div className='navbar-logo'>MelodyHub</div>
-            <div className="navbar-avatar">
-                {intial}
-            </div>
-        </nav>
-    )
+  const userFirstName = "Riya";
+  const initial = userFirstName.charAt(0).toUpperCase();
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-section left">
+        <div className="navbar-logo">MelodyHub</div>
+      </div>
+
+      <div className="navbar-section center">
+        <div className="navbar-search">
+          <MagnifyingGlassIcon className="search-icon" />
+          <input
+            type="text"
+            placeholder="Search Here"
+            className="search-input"
+          />
+        </div>
+      </div>
+
+      <div className="navbar-section right">
+        <div className="navbar-avatar">{initial}</div>
+      </div>
+    </nav>
+  );
 }
