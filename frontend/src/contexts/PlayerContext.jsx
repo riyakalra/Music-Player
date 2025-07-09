ca49001e5e30bfcc59b6fb98492f234a72f5c650
riyakalra <riyakalra59@gmail.com>
2025-07-09 21:15:52 +0530

    enable next and previous song play

--- frontend/src/contexts/PlayerContext.jsx
+++ frontend/src/contexts/PlayerContext.jsx
@@ -1,27 +1,51 @@
-import React, {createContext, useContext, useState} from 'react';
+import React, { createContext, useContext, useState } from "react";
 
 const PlayerContext = createContext();
 
 export const usePlayer = () => useContext(PlayerContext);
 
-export const PlayerProvider = ({children}) => {
-    const [currentSong, setCurrentSong] = useState(null);
-    const [songQueue, setSongQueue] = useState([]);
+export const PlayerProvider = ({ children }) => {
+  const [currentSong, setCurrentSong] = useState(null);
+  const [songQueue, setSongQueue] = useState([]);
 
-    const playNextSong = () => {
-        if (!currentSong || songQueue.length === 0) return;
-        const currentIndex = songQueue.findIndex(song => song.id === currentSong.id);
-        const nextIndex = currentIndex + 1;
-        if (nextIndex < songQueue.length) {
-            setCurrentSong(songQueue[nextIndex]);
-        } else {
-            setCurrentSong(null);
-        }
+  const playNextSong = () => {
+    if (!currentSong || songQueue.length === 0) return;
+    const currentIndex = songQueue.findIndex(
+      (song) => song.id === currentSong.id
+    );
+    const nextIndex = currentIndex + 1;
+    if (nextIndex < songQueue.length) {
+      setCurrentSong(songQueue[nextIndex]);
+    } else {
+      setCurrentSong(null);
     }
+  };
 
-    return (
-        <PlayerContext.Provider value={{currentSong, setCurrentSong, songQueue, setSongQueue, playNextSong}}>
-            {children}
-        </PlayerContext.Provider>
-    )
-}
+  const playPreviousSong = () => {
+    if (!currentSong || songQueue.length === 0) return;
+    const currentIndex = songQueue.findIndex(
+      (song) => song.id === currentSong.id
+    );
+    const previousIndex = currentIndex - 1;
+    if (previousIndex >= 0) {
+      setCurrentSong(songQueue[previousIndex]);
+    } else {
+      setCurrentSong(currentSong);
+    }
+  };
+
+  return (
+    <PlayerContext.Provider
+      value={{
+        currentSong,
+        setCurrentSong,
+        songQueue,
+        setSongQueue,
+        playNextSong,
+        playPreviousSong,
+      }}
+    >
+      {children}
+    </PlayerContext.Provider>
+  );
+};
