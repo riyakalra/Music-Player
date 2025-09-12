48935735eea524639bc0114809c44ae8fe907d79
riyakalra <riyakalra59@gmail.com>
2025-09-12 14:12:21 +0530

    fix: mobile view

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -32,7 +32,7 @@ export default function Navbar() {
         setSearchResults([]);
         setSearchQuery("");
       }
-  
+
       if (
         userDropdownRef.current &&
         !userDropdownRef.current.contains(e.target) &&
@@ -41,22 +41,22 @@ export default function Navbar() {
         setIsDropdownOpen(false);
       }
     };
-  
+
     document.addEventListener("mousedown", handleClickOutside);
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
-  
 
-  const handleLogout = async() => {
+  const handleLogout = async () => {
     try {
       const auth = getAuth();
       await signOut(auth);
-      navigate("/auth")
+      localStorage.removeItem("currentSong");
+      localStorage.removeItem("songQueue");
+      navigate("/auth");
     } catch (error) {
       console.error("Error during logout:", error);
     }
-   
-  }
+  };
 
   const handleSearch = async (query) => {
     setSearchQuery(query);
@@ -82,74 +82,72 @@ export default function Navbar() {
 
   return (
     <>
-    <nav className="navbar">
-      <div className="navbar-section left">
-        <div className="navbar-logo">MelodyHub</div>
-      </div>
-
-      <div className="navbar-section center desktop-search">
-        <div className="navbar-search" ref={inputContainerRef}>
-          <MagnifyingGlassIcon className="search-icon" />
-          <input
-            type="text"
-            placeholder="Search Here"
-            className="search-input"
-            value={searchQuery}
-            onChange={(e) => handleSearch(e.target.value)}
-          />
-          {searchResults.length > 0 && (
-            <XMarkIcon className="clear-icon" onClick={handleClearSearch} />
-          )}
+      <nav className="navbar">
+        <div className="navbar-section left">
+          <div className="navbar-logo">MelodyHub</div>
         </div>
 
-        {searchQuery.length > 0 &&
-          createPortal(
-            <div className="search-results" ref={dropdownRef}>
-              {searchResults.length > 0
-                ? searchResults.map((song) => (
-                    <div
-                      key={song.id}
-                      className="search-result-item"
-                      onClick={() => setCurrentSong(song)}
-                    >
-                      <img
-                        src={song.image}
-                        alt={song.title}
-                        className="result-image"
-                      />
-                      <div className="result-info">
-                        <div className="result-title">{song.title}</div>
-                        <div className="result-artists">
-                          {`${song.album} | ${song.artists.join(", ")}`}
+        <div className="navbar-section center">
+          <div className="navbar-search" ref={inputContainerRef}>
+            <MagnifyingGlassIcon className="search-icon" />
+            <input
+              type="text"
+              placeholder="Search Here"
+              className="search-input"
+              value={searchQuery}
+              onChange={(e) => handleSearch(e.target.value)}
+            />
+            {searchResults.length > 0 && (
+              <XMarkIcon className="clear-icon" onClick={handleClearSearch} />
+            )}
+          </div>
+
+          {searchQuery.length > 0 &&
+            createPortal(
+              <div className="search-results" ref={dropdownRef}>
+                {searchResults.length > 0
+                  ? searchResults.map((song) => (
+                      <div
+                        key={song.id}
+                        className="search-result-item"
+                        onClick={() => setCurrentSong(song)}
+                      >
+                        <img
+                          src={song.image}
+                          alt={song.title}
+                          className="result-image"
+                        />
+                        <div className="result-info">
+                          <div className="result-title">{song.title}</div>
+                          <div className="result-artists">
+                            {`${song.album} | ${song.artists.join(", ")}`}
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  ))
-                : "No search results found."}
-            </div>,
-            document.getElementById("search-portal")
-          )}
-      </div>
-
-      <div className="navbar-section right">
-        <div className="navbar-search mobile-search">
-          <MagnifyingGlassIcon className="search-icon" />
-          <input type="text" placeholder="Search" className="search-input" />
+                    ))
+                  : "No search results found."}
+              </div>,
+              document.getElementById("search-portal")
+            )}
         </div>
-        <div
-          className="navbar-avatar"
-          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-        >
-          {initial}
-        </div>
-      </div>
-    </nav>
 
-    {isDropdownOpen && (
-      <div className="user-dropdown" ref={userDropdownRef}>
-        <div className="dropdown-item" onClick={handleLogout}>Logout</div>
-      </div>
-    )}
+        <div className="navbar-section right">
+          <div
+            className="navbar-avatar"
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+          >
+            {initial}
+          </div>
+        </div>
+      </nav>
+
+      {isDropdownOpen && (
+        <div className="user-dropdown" ref={userDropdownRef}>
+          <div className="dropdown-item" onClick={handleLogout}>
+            Logout
+          </div>
+        </div>
+      )}
     </>
   );
 }
